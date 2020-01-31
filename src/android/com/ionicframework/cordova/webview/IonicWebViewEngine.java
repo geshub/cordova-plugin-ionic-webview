f97a75c13efd3a36fd276d479fe7c92561407667
geshub <ges@site-creator.biz>
2020-01-31 16:45:03 +0000

    text setRenderPriority HIGH (android)

--- src/android/com/ionicframework/cordova/webview/IonicWebViewEngine.java
+++ src/android/com/ionicframework/cordova/webview/IonicWebViewEngine.java
@@ -80,6 +80,8 @@ public class IonicWebViewEngine extends SystemWebViewEngine {
       final WebSettings settings = webView.getSettings();
       int mode = preferences.getInteger("MixedContentMode", 0);
       settings.setMixedContentMode(mode);
+      // Wk test with high priority (deprecated)
+      settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
     }
     SharedPreferences prefs = cordova.getActivity().getApplicationContext().getSharedPreferences(IonicWebView.WEBVIEW_PREFS_NAME, Activity.MODE_PRIVATE);
     String path = prefs.getString(IonicWebView.CDV_SERVER_PATH, null);
